e43df1d25008e50ba75276447870752a3e8a2fbd
Savernish <enbiyacabuk@outlook.com>
2026-04-26 01:46:45 +0300

    Added interrupts

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![no_std]
 #![no_main]
 
-use embedded_hal::delay::DelayNs;
 use embedded_hal::digital::OutputPin;
 use fugit::ExtU32;
 use panic_halt as _;
@@ -11,14 +10,27 @@ use rp2040_hal::{
     pac,
     sio::Sio,
     watchdog::Watchdog,
+    timer::Alarm0,
+    timer::Alarm,
+    pac::interrupt,
     Timer,
 };
+use core::cell::RefCell;
+use core::sync::atomic::{AtomicBool, Ordering};
+use cortex_m::asm;
+
+use critical_section::Mutex;
 
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
 const XTAL_FREQ_HZ: u32 = 12_000_000;
+
+static TICK: AtomicBool = AtomicBool::new(false);
+
+static SHARED_ALARM: Mutex<RefCell<Option<Alarm0>>> = 
+    Mutex::new(RefCell::new(None));
 
 pub enum Led {
     Led1,
@@ -31,29 +43,45 @@ struct LedManager<'a> {
 }
 
 impl<'a> LedManager<'a> {
-    
     fn on(&mut self, led:Led) {
         let pin = &mut self.pins[led as usize];
         let _ = pin.set_high();
     }
-    
     fn off(&mut self, led:Led) {
         let pin = &mut self.pins[led as usize];
         let _ = pin.set_low();
     }
-
     fn set_all_low(&mut self) {
         for pin in self.pins.iter_mut() {
             let _ = pin.set_low();
         }
     }
-
     fn set_all_high(&mut self) {
         for pin in self.pins.iter_mut() {
             let _ = pin.set_high();
         }
-
     }
+    fn chase_step(&mut self, step: u32) {
+        self.set_all_low();
+        match step % 4 {
+            0 => self.on(Led::Led1),
+            1 => self.on(Led::Led2),
+            2 => self.on(Led::Led3),
+            _ => self.set_all_high(),
+        }
+    }
+}
+
+#[interrupt]
+fn TIMER_IRQ_0() {
+    critical_section::with(|cs| {
+        if let Some(alarm) = SHARED_ALARM.borrow(cs).borrow_mut().as_mut() {
+            alarm.clear_interrupt();
+            alarm.schedule(500.millis()).unwrap();
+        }
+    });
+
+    TICK.store(true, Ordering::Relaxed);
 }
 
 #[cortex_m_rt::entry]
@@ -88,24 +116,29 @@ fn main() -> ! {
     // Hardware timer. Ticks at 1 MHz, reads in microseconds.
     let mut timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
 
+    let mut alarm = timer.alarm_0().unwrap();
+
+    alarm.schedule(500.millis()).unwrap();
+    alarm.enable_interrupt();
+
+    critical_section::with(|cs| {
+            SHARED_ALARM.borrow(cs).replace(Some(alarm));
+    });
+    unsafe {
+        pac::NVIC::unmask(pac::Interrupt::TIMER_IRQ_0);
+    }
+
     let mut manager = LedManager {
         pins: [&mut led1, &mut led2, &mut led3],
     };
 
-    // Ignore the unused-import warning from earlier versions.
-    let _ = clocks.system_clock.freq();
+    let mut state: u32 = 0;
 
     loop {
-        manager.set_all_low();
-        manager.on(Led::Led1);
-        timer.delay_ms(500u32);
-        manager.off(Led::Led1);
-        manager.on(Led::Led2);
-        timer.delay_ms(500u32);
-        manager.off(Led::Led2);
-        manager.on(Led::Led3);
-        timer.delay_ms(500u32);
-        manager.set_all_high();
-        timer.delay_ms(300u32);
+        if TICK.load(Ordering::Relaxed) {
+            TICK.store(false, Ordering::Relaxed);
+            manager.chase_step(state);
+            state = state.wrapping_add(1);
+        }   
     }
 }
